92b691c88196ef10d86f7a4f9c57a593e8972409
siwonhan-coding <mercoding@gmail.com>
2024-07-26 18:26:20 -0400

    changed

--- Database-Management-System/Program.cs
+++ Database-Management-System/Program.cs
@@ -13,8 +13,6 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
-//enable google auth
-//adding some changes
 
 var app = builder.Build();
 
